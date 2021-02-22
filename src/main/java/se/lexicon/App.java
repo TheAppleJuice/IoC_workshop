8196a559881d7c78924912989ac3e118db6d0c41
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2021-02-22 11:23:04 +0100

    Loaded IoC using annotation and config class + run

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,16 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.config.StudentConfig;
+import se.lexicon.dao.StudentDao;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(StudentConfig.class);
+        StudentDao studentDao = applicationContext.getBean("studentDao", StudentDao.class);
+
     }
 }
